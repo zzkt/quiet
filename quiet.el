54d4a9c9ca7eaca01170629563845026ecd09a81
nik gaffney <nik@fo.am>
2016-05-08 12:26:24 +0200

    further commentary

--- quiet.el
+++ quiet.el
@@ -27,19 +27,22 @@
 
 ;;; Commentary:
 
-;; disconnect from the online world for a while, possibly reconnecting later
+;; A simple package to disconnect from the online world for a while,
+;; possibly reconnecting later. Any interruptions or distractions
+;; which occur once the command is run are guaranteed to be local.
 ;;
-;; you may need to customize or setq quiet-disconnect and quiet-connect to
-;; the appropriate shell commands to turn your network (interface) on or off
+;; 'M-x quiet' will disconnect from the network, optionally
+;; reconnecting after a certain time.
 ;;
-;; MacOS
-;;  % networksetup -setairportpower airport {on,off}
-;; Linux 
-;;  % ifup wlan0
-;;  % ifdown wlan0
-;; BSD
-;;  % service netif {stop, start}
-
+;; the function 'quiet' can be used anywhere in emacs where a lack of
+;; network access could be seen as a feature, e.g. as a mode-hook (or
+;; with defadvice) to your preferred distraction free writing
+;; environment.
+;;
+;; you may need to customize or setq quiet-disconnect and
+;; quiet-connect to the appropriate shell commands to turn your
+;; network (interface) on or off
+;;
 ;;; Code:
 
 (provide 'quiet)
